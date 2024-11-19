82a8bce2c1f424eaa12a236cf07228d26c07b06b
¨LuisEUM¨ <luiseum95@gmail.com>
2024-11-19 16:58:58 +0100

    Improving ux/ui and corrections of students

--- road-fighter-HJ/src/scenes/PreloadScene.js
+++ road-fighter-HJ/src/scenes/PreloadScene.js
@@ -5,11 +5,11 @@ export default class PreloadScene extends Phaser.Scene {
 
     preload() {
         // Load game assets with correct paths
-        this.load.image('player', '/assets/images/sprites/player.png');
-        this.load.image('enemy-blue', '/assets/images/sprites/enemy-blue.png');
-        this.load.image('enemy-yellow', '/assets/images/sprites/enemy-yellow.png');
-        this.load.image('enemy-green', '/assets/images/sprites/enemy-green.png');
-        this.load.image('road', '/assets/images/backgrounds/road.png');
+        this.load.image('player', './assets/images/sprites/player.png');
+        this.load.image('enemy-blue', './assets/images/sprites/enemy-blue.png');
+        this.load.image('enemy-yellow', './assets/images/sprites/enemy-yellow.png');
+        this.load.image('enemy-green', './assets/images/sprites/enemy-green.png');
+        this.load.image('road', './assets/images/backgrounds/road.png');
     }
 
     create() {
